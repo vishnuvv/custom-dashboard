d2d10c08399e58703f1d4e22ad3d59461e8081b7
Vakkalagadda Vishnu Vardhan <vvi100@cambiumnetworks.com>
2022-04-18 18:09:44 +0530

    Added semantic css file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Header } from "semantic-ui-react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header as="h3">Customdashboard</Header>
     </div>
   );
 }
