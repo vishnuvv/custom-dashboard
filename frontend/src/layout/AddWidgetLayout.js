082e550f5707ff599468cd577e213588f420bf82
Radhakrishnaiah Vedimala <rve100@cambiumnetworks.com>
2022-04-20 00:05:33 +0530

    add item button added

--- frontend/src/layout/AddWidgetLayout.js
+++ frontend/src/layout/AddWidgetLayout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import _ from "lodash";
 import RGL, { WidthProvider } from "react-grid-layout";
+import { Button } from "semantic-ui-react";
 import WidgetComponent from "./WidgetComponent.js";
 import { connect } from "react-redux";
 const ReactGridLayout = WidthProvider(RGL);
 
-class AddWidgetLayout extends React.Component {
+class AddWidgetLayout extends React.PureComponent {
   static defaultProps = {
     className: "layout",
     items: 1,
@@ -17,22 +18,39 @@ class AddWidgetLayout extends React.Component {
   constructor(props) {
     super(props);
     const layout = this.generateLayout();
-    this.state = { layout };
+    this.state = { layout, items: 0 };
+    this.onAddItem = this.onAddItem.bind(this);
   }
   generateDOM() {
     const customWidgets = this?.props?.customWidgets;
-    return _.map(
-      _.range(this?.props?.customWidgets.length || this.props.items),
-      function (i) {
-        return (
-          <div key={i}>
-            <span>
-              <WidgetComponent index={i} customWidgets={customWidgets} />
-            </span>
-          </div>
-        );
-      }
-    );
+    return _.map(_.range(this.state.items), function (i) {
+      return (
+        <div key={i}>
+          <span>
+            <WidgetComponent index={i} customWidgets={customWidgets} />
+          </span>
+        </div>
+      );
+    });
+  }
+
+  onAddItem() {
+    localStorage.removeItem("createCustomWidget.stepOne");
+    localStorage.removeItem("createCustomWidget.stepTwo");
+    /*eslint no-console: 0*/
+    //console.log("adding", "n" + this.state.newCounter);
+    this.setState({
+      // Add a new item. It must have a unique key!
+      // items: this.state.items.concat({
+      //   i: "n" + this.state.newCounter,
+      //   x: (this.state.items.length * 2) % (this.state.cols || 12),
+      //   y: Infinity, // puts it at the bottom
+      //   w: 2,
+      //   h: 2,
+      // }),
+      // Increment the counter to ensure key is always unique.
+      items: this.state.items + 1,
+    });
   }
 
   generateLayout() {
@@ -55,13 +73,21 @@ class AddWidgetLayout extends React.Component {
 
   render() {
     return (
-      <ReactGridLayout
-        layout={this.state.layout}
-        onLayoutChange={this.onLayoutChange}
-        {...this.props}
-      >
-        {this.generateDOM()}
-      </ReactGridLayout>
+      <>
+        <Button
+          onClick={this.onAddItem}
+          style={{ margin: "0 auto", display: "block" }}
+        >
+          Add Item
+        </Button>
+        <ReactGridLayout
+          layout={this.state.layout}
+          onLayoutChange={this.onLayoutChange}
+          {...this.props}
+        >
+          {this.generateDOM()}
+        </ReactGridLayout>
+      </>
     );
   }
 }
