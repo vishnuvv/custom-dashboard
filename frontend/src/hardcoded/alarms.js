9059f024a07afa5738f9e09eb43fed3cf8bce8f0
Vakkalagadda Vishnu Vardhan <vvi100@cambiumnetworks.com>
2022-04-19 12:29:03 +0530

    Added vega render

---
+++ frontend/src/hardcoded/alarms.js
@@ -0,0 +1,154 @@
+export const alarms = [
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+    Time: 1650182400000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+    Time: 1650186000000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+    Time: 1650189600000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+    Time: 1650193200000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+    Time: 1650196800000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+    Time: 1650200400000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+    Time: 1650204000000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+    Time: 1650207600000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+    Time: 1650211200000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+    Time: 1650214800000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+    Time: 1650218400000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+    Time: 1650222000000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+    Time: 1650225600000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+
+    Time: 1650229200000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+
+    Time: 1650232800000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+
+    Time: 1650236400000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+
+    Time: 1650240000000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+
+    Time: 1650243600000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+
+    Time: 1650247200000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+
+    Time: 1650250800000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+
+    Time: 1650254400000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+    Time: 1650258000000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+    Time: 1650261600000,
+  },
+  {
+    Minor: 18,
+    Major: 723,
+    Critical: 2,
+    Time: 1650265200000,
+  },
+];
